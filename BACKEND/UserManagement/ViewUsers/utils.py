5b799ac66f6c30cd15744385b01f493258ded210
JordinPinzon <jordinhernanpinzon@gmail.com>
2025-01-27 15:06:33 -0500

    feat: :lock: Fix in user validation so that only administrators can enter

--- BACKEND/UserManagement/ViewUsers/utils.py
+++ BACKEND/UserManagement/ViewUsers/utils.py
@@ -11,12 +11,15 @@ ALGORITHM = os.getenv("ALGORITHM")
 
 async def verify_admin(request: Request):
     token = request.headers.get("Authorization")
-    if not token:
-        raise HTTPException(status_code=401, detail="Token no proporcionado")
+    if not token or not token.startswith("Bearer "):
+        raise HTTPException(status_code=401, detail="Token no proporcionado o mal formado")
 
     try:
-        payload = jwt.decode(token.split(" ")[1], SECRET_KEY, algorithms=[ALGORITHM])
-        if payload.get("role_id") != 1:  # Validar si es administrador
+        token = token.split(" ")[1]
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+
+        # Verificar si el rol es de administrador
+        if payload.get("role_id") != 1:  # role_id debe ser 1 para administrador
             raise HTTPException(status_code=403, detail="Acceso denegado. Solo para administradores.")
     except jwt.ExpiredSignatureError:
         raise HTTPException(status_code=401, detail="Token expirado")
