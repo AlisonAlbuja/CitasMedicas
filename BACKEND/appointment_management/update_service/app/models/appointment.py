ade7811d1650a618ec35cfbe476142a958545e3e
Alison <134740024+AlisonAlbuja@users.noreply.github.com>
2025-01-29 18:01:51 -0500

    feat: :zap: Creating the Edit Citations Microservice

---
+++ BACKEND/appointment_management/update_service/app/models/appointment.py
@@ -0,0 +1,10 @@
+from pydantic import BaseModel, Field
+from typing import Optional
+from datetime import datetime
+
+class Appointment(BaseModel):
+    id: Optional[int] = None
+    title: str = Field(..., min_length=3, max_length=100)
+    description: str = Field(..., min_length=5, max_length=500)
+    date: datetime
+    time: str  # Puedes cambiar a datetime.time si lo deseas
