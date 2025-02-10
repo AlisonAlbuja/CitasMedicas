3602e9f09d48c6a2ce25baf457c1b5848ca47ef8
Alison <134740024+AlisonAlbuja@users.noreply.github.com>
2025-02-10 16:40:26 -0500

    feat: :sparkles: Add comments in the read microservice to test the action

--- BACKEND/appointment_management/read_service/app/routers/main.py
+++ BACKEND/appointment_management/read_service/app/routers/main.py
@@ -2,12 +2,12 @@ from fastapi import FastAPI
 from fastapi.responses import JSONResponse
 from app.routers.read import router as read_router
 
-app = FastAPI(title="Read Service", description="Servicio para leer citas", version="1.0.0")
+app = FastAPI(title="Read Service", description="Service for reading quotes", version="1.0.0")
 
-# Endpoint raíz para verificar el estado del servicio
+# Root endpoint to check service status
 @app.get("/")
 def root():
     return JSONResponse({"message": "Read service is running successfully."})
 
-# Incluir el router de lectura
+# Include the reading router
 app.include_router(read_router, prefix="/api/v1")
